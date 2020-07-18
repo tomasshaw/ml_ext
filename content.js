dca96d279177ad44cb03a11d071accde9d101639
Thomas Shaw <tomasshaw@outlook.com>
2020-07-18 13:28:17 -0300

    feat(project): add style changes to widget. updated readme

--- content.js
+++ content.js
@@ -29,22 +29,73 @@ async function createNewComments(textArray) {
 		textArray.map(async (word) => {
 			const matched = word.match('articulo.mercadolibre.com.ar')
 			if (matched) {
+				const itemLink = matched.input
 				const [itemPrice, itemName, itemPicture] = await getProductInfo(
-					matched.input
+					itemLink
 				)
-				return `${itemName}, $${itemPrice}, <img src=${itemPicture}>`
+				return createHTMLWidget({ itemPrice, itemName, itemPicture, itemLink })
 			}
 			const newMatched = word.match('[p][/]{1}[MLml]{2}[A-z]{1}[0-9]+')
 			if (newMatched) {
+				const itemLink = newMatched.input
 				const [itemPrice, itemName, itemPicture] = await getProductInfoWithApi(
 					newMatched[0].split('/').pop()
 				)
-				return `${itemName}, $${itemPrice}, <img src=${itemPicture}>`
+				return createHTMLWidget({ itemPrice, itemName, itemPicture, itemLink })
 			}
 			return word
 		})
 	)
 	return newCommentArray
+}
+
+function createHTMLWidget({ itemPrice, itemName, itemPicture, itemLink }) {
+	const htmlWidget = `
+		<style>
+			.wrapperDiv {
+				margin: 4px;
+				width: 500px;
+				height: 70px;
+				border: 1px solid;
+				border-color: rgb(245,245,245);
+				border-radius: 4px;
+				box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+				display: flex;
+			}
+			.ml_widget_picture {
+				width: 70px;
+				height: 70px;
+				white-space: nowrap;
+				text-align: center;
+				margin-right: 8px;
+				border-right: 1px solid;
+				border-right-color: rgba(51, 51, 51, 0.1);
+			}
+			.ml_name_and_title {
+				display: flex;
+				flex-direction: column;
+			}
+			.productImage {
+				max-width: 65px;
+				max-height: 65px;
+				vertical-align: middle;
+			}
+			.img_helper{
+				display: inline-block;
+				height: 100%;
+				vertical-align: middle;
+			}
+		</style>
+		<a href="${itemLink}" target="_blank" class="wrapperDiv">
+		<div class="ml_widget_picture">
+			<span class="img_helper"></span>
+			<img class="productImage" src=${itemPicture}>
+		</div>
+		<div class="ml_name_and_title">
+			<h5>${itemName}</h5><h5>$${itemPrice}</h5>
+		</div>
+		</a>`
+	return htmlWidget
 }
 
 async function getProductInfoWithApi(match) {
